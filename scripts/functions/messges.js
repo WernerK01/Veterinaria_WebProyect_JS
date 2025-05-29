c8a4d8803ac361308cfbfd0facfe87dda9e49a3c
Werner Koll <wernerkoll01@gmail.com>
2025-05-29 18:17:37 -0300

    Remove Svg's & Update script and messages.

--- scripts/functions/messges.js
+++ scripts/functions/messges.js
@@ -7,8 +7,9 @@ export function MessageSucces(args) {
         position: 'right',
         stopOnFocus: true,
         style: {
-            background: 'linear-gradient(to right, #31e2b4, #5A8)',
-            color: "#ffffff",
+            background: '#0F0F1A',
+            color: "#31e2b4",
+            borderColor: '#31e2b4',
             borderRadius: "5px"
         }
     }).showToast();
@@ -17,15 +18,32 @@ export function MessageSucces(args) {
 export function MessageError(args) {
     Toastify({
         text: args,
-        avatar: '../img/x-circle.svg',
         duration: 3000,
         close: true,
         gravity: 'top',
         position: 'right',
         stopOnFocus: true,
         style: {
-            background: 'linear-gradient(to right, #F02040,rgb(165, 20, 42))',
-            color: "#ffffff",
+            background: ' #0F0F1A',
+            color: "#F02040",
+            borderColor: '#F02040',
+            borderRadius: "5px"
+        }
+    }).showToast();
+}
+
+export function MessageWarning(args) {
+    Toastify({
+        text: args,
+        duration: 3000,
+        close: true,
+        gravity: 'top',
+        position: 'right',
+        stopOnFocus: true,
+        style: {
+            background: ' #0F0F1A',
+            color: '#FC2',
+            borderColor: '#FC2',
             borderRadius: "5px"
         }
     }).showToast();
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 import { BtnSearchToggler } from './functions/searchBtn.js';
 import { LocalStorageAdd, LocalStorageClear } from './functions/localStorege.js';
-import { MessageSucces, MessageError } from './functions/messges.js'
+import { MessageSucces, MessageError, MessageWarning } from './functions/messges.js'
  
 const btnSearchToggle = document.querySelector('#btnSearchToggle');
 const btnSearch = document.querySelector('#btnSearch');
@@ -43,6 +43,7 @@ btnGlobalClearPet.addEventListener('click', () => {
     try {
         LocalStorageClear('Pets', pets);
     } catch(err) {
-        console.error(`[ERROR]: Se generó un error en 'btnGlobalClearPet-Event-Click': ${err.message}\n${err}`)
+        console.error(`[ERROR]: Se generó un error en 'btnGlobalClearPet-Event-Click': ${err.message}\n${err}`);
+        MessageError(`${err.message}`);
     }
 });
