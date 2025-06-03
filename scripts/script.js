8ee6c68dfb6f25f8d20ae5237d5d42ae5aa9ad6d
Werner Koll <wernerkoll01@gmail.com>
2025-06-02 23:26:15 -0300

    Added remove client script

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
 import './global/UI/others/btnClearLocalStorege.js';
 import './global/UI/others/btnSearch.js';
 import './global/UI/user/btnClientAdd.js';
+import './global/UI/user/btnClientRemove.js';
